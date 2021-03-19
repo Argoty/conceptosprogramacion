0f928909fbc1a6838afae5f20e758d362cde4a23
JaxisArty <javierargoty35@gmail.com>
2021-03-18 20:31:41 -0500

    javierargoty35@gmail.com

---
+++ main.js
@@ -0,0 +1,12 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+});
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.nav-main', {delay:200});
+ScrollReveal().reveal('.showcase', {delay:200});
+ScrollReveal().reveal('.articulo', {delay:200});
+ScrollReveal().reveal('.banner-one', {delay:200});
+ScrollReveal().reveal('.banner-two', {delay:200});
+ScrollReveal().reveal('.section-redes', {delay:200});
+ScrollReveal().reveal('.footer-links', {delay:200});
+ScrollReveal().reveal('.footer', {delay:200});
